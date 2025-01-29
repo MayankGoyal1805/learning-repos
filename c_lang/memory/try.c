edaedd2131afe11c3d8868a97590d6594412fa90
MayankGoyal1805 <178543798+MayankGoyal1805@users.noreply.github.com>
2025-01-29 21:14:50 +0530

    Intitial commit

---
+++ c_lang/memory/try.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+void test();
+int global;
+int main()
+{
+    int *ptr;
+    int i;
+    static int st;
+    ptr = (int *)malloc(sizeof(int)); /* Allocate memory from the heap. */
+    if (ptr != NULL)
+    {
+        printf("Code seg: %p\n", test);
+        printf("Data seg: %p %p\n", &global, &st);
+        printf("Stack seg: %p\n", &i);
+        printf("Heap: %p\n", ptr);
+        free(ptr);
+    }
+    return 0;
+}
+void test()
+{
+}
